ee85d335fd772ee3faec21cdc97f6ab770f4957d
Siddhi Jaiswal <siddhijaiz23@gmail.com>
2025-05-17 17:17:08 +0530

    deleted node_modules and package_json-lock and reinstalled dependencies

--- backend/Routes/User.js
+++ backend/Routes/User.js
@@ -100,7 +100,8 @@ router.post('/login', async (req, res)=>{
             const token = jwt.sign(payload, process.env.JWT_SECRET, {expiresIn:"5h"});
             // store token in cokkie
             res.cookie('Token', token, {
-                httpOnly: false, 
+                // httpOnly: false, 
+                httpOnly: true,
                 maxAge:5 * 60 * 60 * 1000,
                 sameSite: 'None', 
                 secure: true,
