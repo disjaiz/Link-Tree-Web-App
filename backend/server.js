1fbbc9c957bfe70ddd8901c23172c52a2fed5cf1
Siddhi Jaiswal <siddhijaiz23@gmail.com>
2025-05-17 17:52:13 +0530

    entered right new url to cors

--- backend/server.js
+++ backend/server.js
@@ -17,28 +17,28 @@ app.use(express.json());
 //   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 //   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 // }));
-// app.use(cors({
-//   origin: 'http://192.168.0.105:5173', 
-//   credentials: true, 
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
-// }));
-
-const allowedOrigins = ['https://link-tree-web-app-1.onrender.com','http://localhost:5173', 'http://192.168.0.105:5173'];
-
 app.use(cors({
-  origin: function(origin, callback) {
-    if (allowedOrigins.includes(origin) || !origin) {
-      // allow requests with no origin (like mobile devices)
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
+  origin: 'https://link-tree-web-app-2-backend.onrender.com', 
+  credentials: true, 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }));
+
+// const allowedOrigins = ['https://link-tree-web-app-1.onrender.com','http://localhost:5173', 'http://192.168.0.105:5173'];
+
+// app.use(cors({
+//   origin: function(origin, callback) {
+//     if (allowedOrigins.includes(origin) || !origin) {
+//       // allow requests with no origin (like mobile devices)
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   credentials: true,
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+// }));
 
 
 app.use(cookieParser());
