077b9159700e019a5f73fba9042d03cad4fe8f1e
Siddhi Jaiswal <siddhijaiz23@gmail.com>
2025-12-09 14:59:57 +0530

    Stop tracking env and config files

--- frontend/src/config.js
+++
@@ -1,6 +0,0 @@
-const baseUrl = import.meta.env.MODE === 'development'? 'http://localhost:3000': 'https://link-tree-web-app-2-backend.onrender.com';
-
-const frontEndBaseUrl  =import.meta.env.MODE  === 'development'? "http://localhost:5173" : "https://link-tree-web-app-frontend.onrender.com";
-
-export default baseUrl;
-export { frontEndBaseUrl };
